4ba9e33fbaf43f200a862ed45cae9f2aa47dbcbd
ibarbylev <ibarbylev@gmail.com>
2022-03-21 01:22:24 +0200

    add admin

--- authors/models.py
+++ authors/models.py
@@ -6,7 +6,13 @@ class Author(models.Model):
     last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField()
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}: {self.birthday_year}'
+
 
 class Book(models.Model):
     authors = models.ManyToManyField(Author)
     title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{[author for author in self.authors.all()]} {self.title}'
